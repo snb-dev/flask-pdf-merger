412be83af0ebad19b934d9c06608b9860e69916d
snb-dev <snb10snb@gmail.com>
2025-02-15 08:32:06 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, send_file
+import PyPDF2
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "PDF Merger API is Running!"
+
+@app.route("/merge", methods=["POST"])
+def merge_pdfs():
+    uploaded_files = request.files.getlist("pdfs")
+    if len(uploaded_files) < 2:
+        return "Upload at least two PDFs.", 400
+
+    merger = PyPDF2.PdfMerger()
+    for file in uploaded_files:
+        merger.append(file)
+
+    output_path = "merged_output.pdf"
+    merger.write(output_path)
+    merger.close()
+
+    return send_file(output_path, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
